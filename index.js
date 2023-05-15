b2374e97cb4fcb971ffb79998b141f803093a973
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-15 16:38:32 -0700

    JavaScript in Browsers

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot cotain a space or hyohen (-)
+// Are case-sensitive
+
+let firstName;
