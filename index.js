de7e4943db85336f4dcb1cf13e86a0b334703857
NathanAguilar <nathan.rene.aguilar@gmail.com>
2023-05-15 16:56:52 -0700

    Variables

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-let name = 'Mosh';
-console.log(name);
+// let name = 'Mosh';
+// console.log(name);
 
 // Cannot be a reserved keyword
 // Should be meaningful
@@ -8,4 +8,8 @@ console.log(name);
 // Cannot cotain a space or hyohen (-)
 // Are case-sensitive
 
-let firstName;
+// let firstName;
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
